0403a1faf5004f0643391049b0749833ce323603
Jan Godziatkowski <ph.godzio@gmail.com>
2015-03-12 23:59:22 +0100

    Konstruktor klasy Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -15,6 +15,11 @@ public class Money {
     private String currency;
     private BigDecimal value;
 
+    public Money(String currency, BigDecimal value) {
+        this.currency = currency;
+        this.value = value;
+    }
+    
     public String getCurrency() {
         return currency;
     }
